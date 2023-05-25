7ff9a3399c450cec9717b1e3295479f363fa571e
Jiyeon Heo <jiyeon.heo0906@gmail.com>
2023-05-24 19:47:36 -0600

    Initial commit

---
+++ Lab/Lab00.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    public class Lab00
+    {
+        public static void Main(String[] args)
+        {
+
+            int lowNumber = 0;
+            int highNumber = 0;
+
+            bool activeLow = true;
+            while (activeLow)
+            {
+                Console.WriteLine("Enter the low number. ");
+                lowNumber = Convert.ToInt32(Console.ReadLine());
+                if (lowNumber > 0)
+                {
+                    activeLow = false;
+                }
+            }
+
+
+            bool activeHigh = true;
+            while (activeHigh)
+            {
+                Console.WriteLine("Enter the high number. ");
+                highNumber = Convert.ToInt32(Console.ReadLine());
+                if (highNumber > lowNumber)
+                {
+                    activeHigh = false;
+                }
+            }
+
+
+            Console.Write("Difference between the low and high variables is : ");
+            Console.WriteLine(highNumber - lowNumber);
+
+            int[] nums = new int[highNumber - lowNumber - 1];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                highNumber -= 1;
+                nums[i] = highNumber;
+                Console.WriteLine(nums[i]);
+            }
+
+
+            string[] changeToString = Array.ConvertAll(nums, num => num.ToString());
+            
+            string filepath = @"C:\Users\jiyeo\OneDrive\Desktop\CPRG211_B\Lab\Lab\numbers.txt";
+
+            FileStream fs = File.Create(filepath);
+            fs.Close();
+            File.WriteAllLines(filepath, changeToString);
+            fs.Close();
+
+
+            //additional Tasks
+            string[] readFromFile = File.ReadAllLines(filepath);
+            fs.Close();
+
+            //foreach(string line in readFromFile)
+            //{
+            //    Console.WriteLine(line);
+            //}
+
+            int[] sumFromFile = Array.ConvertAll(readFromFile, int.Parse);
+            Console.WriteLine("The sum of the numbers between highest and lowest is " + sumFromFile.Sum());
+
+        }
+    }
+}
---
+++ Lab/Lab01.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab
+{
+    public class Person
+    {
+        private int _personId;
+        private string _firstName;
+        private string _lastName;
+        private string _favoriteColour;
+        private int _age;
+        private bool _isWorking;
+
+        public int PersonId
+        {
+            get { return _personId; }
+            set { _personId = value; }
+        }
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string FavoriteColour
+        {
+            get { return _favoriteColour; }
+            set { _favoriteColour = value; }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set { _age = value; }
+        }
+        public bool IsWorking
+        {
+            get { return _isWorking; }
+            set { _isWorking = value; }
+        }
+
+        public Person()
+        {
+            //this._personId = pid;
+        }
+        public Person(int pid, string firstname, string lastname, string fcolor, int age, bool working)
+        {
+            this._personId = pid;
+            this._firstName = firstname;
+            this._lastName = lastname;
+            this._favoriteColour = fcolor;
+            this._age = age;
+            this._isWorking = working;
+        }
+
+        public void DisplayPersonInfo()
+        {
+            Console.WriteLine(PersonId + ":" + FirstName + LastName + "'s favoite color is " + FavoriteColour);
+        }
+
+        public string ChangeFavoriteColour()
+        {
+            FavoriteColour = "White";
+            return FavoriteColour;
+        }
+        public int GetAgeInTenYears()
+        {
+            int age = Age + 10;
+            return age;
+        }
+        public override string ToString()
+        {
+            // display all Person Object information as a list
+            //return "PersonId: "+PersonId+"\nFirstName: "+ FirstName+"\nLastName: "+LastName+"\nFavoriteColour: " + FavoriteColour + "\nAge: " + Age+"\nIsWorking: "+ IsWorking ;
+            string result = $"PersonId: {PersonId}\n";
+            result += $"FirstName: {FirstName}\n";
+            result += $"LastName: {LastName}\n";
+            result += $"FavoriteColour: {FavoriteColour}\n";
+            result += $"Age: {Age}\n";
+            result += $"IsWorking: {IsWorking}";
+
+            return result;
+        }
+    }
+
+    public class Relation
+    {
+        public enum RelationType
+        {
+            Sister, Brother, Mother, Father
+        }
+        public void ShowRelationShip(Person p1, Person p2)
+        {
+            if (p1.FirstName == "Gina" && p2.FirstName == "Mary")
+            {
+                Console.WriteLine("Relationship between Gina and Mary is: " + RelationType.Sister);
+            }
+            else if (p1.FirstName == "Ian" && p2.FirstName == "Mike")
+            {
+
+                Console.WriteLine("Relationship between Ian and Mike is: " + RelationType.Brother);
+            }
+        }
+
+    }
+
+    public class MainLab
+    {
+        public static void Main(string[] args)
+        {
+            Person p1 = new Person(1, "Ian", "Brooks", "Red", 30, true);
+            Person p2 = new Person(2, "Gina", "James", "Green", 18, false);
+            Person p3 = new Person(3, "Mike", "Briscoe", "Blue", 45, true);
+            Person p4 = new Person(4, "Mary", "Beals", "Yellow", 28, true);
+            Person p5 = new Person();
+
+            p2.DisplayPersonInfo();
+            Console.WriteLine(p3.ToString());
+            p1.ChangeFavoriteColour();
+
+            Relation relation1 = new Relation();
+            Relation relation2 = new Relation();
+            relation1.ShowRelationShip(p2, p4);
+            relation2.ShowRelationShip(p1, p3);
+
+            p1.DisplayPersonInfo();
+            Console.WriteLine(p4.FirstName + " " + p4.LastName + "'s Age in 10 year is: " + p4.GetAgeInTenYears());
+
+            List<Person> list = new List<Person>();
+            list.Add(p1);
+            list.Add(p2);
+            list.Add(p3);
+            list.Add(p4);
+
+            double averageAge = list.Average(person => person.Age);
+            Console.WriteLine("Average age is: " + averageAge);
+
+            //shows only ages
+            //int youngest = list.Min(person => person.Age); 
+            //int oldest = list.Max(person => person.Age);
+
+            //dosen't work......
+            //list.OrderBy(person => person.Age);
+            //string youngest = list.First().FirstName;
+            //string oldest = list.Last().FirstName;
+            //Console.WriteLine("The younget person is: " + youngest);
+            //Console.WriteLine("The oldest person is: " + oldest);
+
+            string youngestName = list.OrderBy(person => person.Age).First().FirstName;
+            string oldestName = list.OrderBy(p => p.Age).Last().FirstName;
+            Console.WriteLine("The younget person is: " + youngestName);
+            Console.WriteLine("The oldest person is: " + oldestName);
+
+            //List<Person> searchPeople = new List<Person>();
+            //foreach(Person person in list)
+            //{
+            //    if (person.FirstName.StartsWith("M"))
+            //    {
+            //        searchPeople.Add(person);
+            //    }
+            //}
+
+            List<Person> searchPeople = list.Where(p => p.FirstName.StartsWith("M")).ToList();
+
+            foreach (Person person in searchPeople)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
+            List<Person> likeBlue = list.Where(person => person.FavoriteColour == "Blue").ToList();
+
+            foreach (Person person in likeBlue)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
+        }
+    }
+}
